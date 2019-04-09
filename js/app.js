ef4d705b2ce8f5688cfdabf5d7e1e90a771dd5d2
Anthony Eriksen <ajrayid@gmail.com>
2019-04-09 00:52:13 -0600

    Adding Modal
    Working on adding a modal.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
+let randomUsers = [];
+const modalContent = document.querySelector('.modal-content');
 
-
-// Load Random Users
 function loadUsers(){
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://randomuser.me/api?nat=us&results=12', true)
@@ -8,26 +8,56 @@ function loadUsers(){
   xhr.onload = function(){
     if(this.status == 200){
       const users = JSON.parse(this.responseText);
-      console.log(users.results);
+      let results = users.results;
+      console.log(results);
 
+      
       let output = '';
-      for(let i in users.results) {
+      let randomUser = output;
+      for(let i in results) {
         output +=
         '<div class="'+"user "+"user-"+[i]+'">'+
-        '<img class ="userImg" src=' +users.results[i].picture.large+ '>' +
+        '<img class ="userImg" src=' +results[i].picture.large+ '>' +
         '<ul class="userInfo">'+
-        '<li><h3>'+users.results[i].name.first+' '+users.results[i].name.last+'</h3></li>'+
-        '<li>'+users.results[i].email+'</li>'+
-        '<li class="city">'+users.results[i].location.city+'</li>'+
+        '<li><h3>'+results[i].name.first+' '+results[i].name.last+'</h3></li>'+
+        '<li>'+results[i].email+'</li>'+
+        '<li class="city">'+results[i].location.city+'</li>'+
         '</ul>'+
         '</div>';
+        
+        
+          // randomUser +=
+          // '<img class ="userImg" src=' +results[i].picture.large+ '>'
+          // image: results[i].picture.large,
+          // name: results[i].name.first+' '+results[i].name.last,
+          // email: results[i].email,
+          // cell: results[i].cell,
+          // address: results[i].location.street+' '+results[i].location.city+','+results[i].location.state,
+          // birthdate: results[i].dob.age
+        ;
+        randomUsers.push(randomUser);
       }
       document.getElementById('usersList').innerHTML = output;
+      const modal = document.querySelector('.modal');
+      const card = document.querySelectorAll('.user');
+
+        for(let i = 0; i < card.length; i++){         // Loop through Nodelist
+        card[i].addEventListener('click', () => {     // Add click even to each item in Nodelist
+          modal.style.display = 'block';             // Change Modal display property to 'block'
+          modalContent.innerHTML = randomUser;
+        });
+      }                                             //End of Modal Click Event
     }
+  //  for (let i =0; i < randomUsers.length; i++) {
+  //     console.log(randomUsers[i]);
+  //     modalContent.innerHTML = randomUsers[i];
+  //  }
   }
   xhr.send();
 }
 loadUsers()
+
+
 
 
 
