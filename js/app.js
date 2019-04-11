f4bc3b95c654bf644825909f7b7c52cf1bea2ff7
Anthony Eriksen <ajrayid@gmail.com>
2019-04-10 18:06:15 -0600

    Got modal working, finishing touches
    Finally figured out the modal funtionality. Cleaned up code a bit, added some finishing touches.

--- js/app.js
+++ js/app.js
@@ -1,72 +1,68 @@
-
-const modalContent = document.querySelector('.modal-content');
-
+let index = document.querySelectorAll('[data-index]');
+let currentUser = [];
 
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://randomuser.me/api?nat=us&results=12', true)
+  xhr.open('GET', 'https://randomuser.me/api?nat=us&results=12', true);
 
   xhr.onload = function(){
     if(this.status == 200){
       const users = JSON.parse(this.responseText);
       let results = users.results;
-      console.log(results);
 
-      // Main Content
       let output = '';
       for(let i in results) {
+        const indexValue = [i];
+        const userPic = results[i].picture.large;
+        const firstName = results[i].name.first;
+        const lastName = results[i].name.last;
+        const userEmail = results[i].email;
+        const userCity = results[i].location.city;
+        const userState = results[i].location.state;
+        const userStreet = results[i].location.street;
+        const userPost = results[i].location.postcode;
+        const userCell = results[i].cell;
+        const userDOB = `Birthday: ` + results[i].dob.date.substring(5,7) + '/' + results[i].dob.date.substring(8,10) + '/' + results[i].dob.date.substring(0,4);
         output +=
-        '<div class="user" id='          +[i]+'>'  +
-        '<img class ="userImg" src='     +results[i].picture.large+ '>' +
-        '<ul class="userInfo">'          +
-        '<li><h3>'                       +results[i].name.first+' '+results[i].name.last+'</h3></li>'+
-        '<li>'                           +results[i].email+'</li>'+
-        '<li class="city">'              +results[i].location.city+'</li>'+
-        '</ul>'                          +
-        '</div>';
+        `<div class = user data-index=${indexValue}>`  +
+        `<img class = userImg src= ${userPic}>` +
+        `<ul class = userInfo>`+
+        `<li><h3 class = userName>${firstName} ${lastName}</h3></li>`+
+        `<li class = userEmail>${userEmail}</li>`+
+        `<li class =city>${userCity}</li>`+
+        `</ul>`+
+        `</div>`;
+
+      currentUser.push(results);
+      document.getElementById('usersList').innerHTML = output;  
+
+        const modalContent = document.querySelector('.modal-content');
+        const modal = document.querySelector('.modal');
+        const card = document.querySelectorAll('.user');
+          for(let i = 0; i < card.length; i++) {         
+            card[i].addEventListener('click', () => {
+                modal.style.display = "block";
+                modalContent.innerHTML = 
+                `<span class="close">&times;</span>` +
+                card[i].innerHTML +`
+                <hr id= modal-hr>
+                <p class= modal-cell>${userCell}</p>
+                <p class= modal-address>${userStreet}  ${userCity}, ${userState} ${userPost}</p>
+                <p class= modal-dob>${userDOB}</p>
+                ` 
+                document.querySelector('.close').addEventListener('click', () => {
+                  modal.style.display = "none";
+                });       
+            });
+          }       
+      } 
+    }
+  };
+xhr.send();
+
+  
+
+  
+
        
-      };
-      document.getElementById('usersList').innerHTML = output;
+
         
-
-      // Modal Content
-      let randomUser = '';
-        for(let i in results) {
-          randomUser +=
-          '<div class="modal-user">'    + 
-          '<img class="modal-img" src=' + results[i].picture.large + '>' +
-          '<h4 class="modal-name">'     + results[i].name.first+' '+results[i].name.last + '</h4>' +
-          '<ul class="modal-info">'     +
-          '<li class="modal-email">'    + results[i].email + '</li>' +
-          '<li class="modal-city">'     + results[i].location.city + '</li>' +
-          '</ul>'                       +
-          '<hr>'                        +
-          '<ul class="modal-contact">'  +
-          '<li class="modal-address">'  + results[i].location.street+' '+results[i].location.city+','+results[i].location.state + '</li>'
-          '<li class="modal-cell">'     + results[i].cell + '</li>'  +
-          '<li class="modal-birthday">' + results[i].dob.age + '</li>' +
-          '</div>';
-        };
-      
-      modalContent.innerHTML = randomUser;
-
-        // Click on a card to open Modal
-      const modal = document.querySelector('.modal');
-      const card = document.querySelectorAll('.user');
-        for(let i = 0; i < card.length; i++){         
-        card[i].addEventListener('click', () => {     
-          modal.style.display = 'block';             
-        });
-      };                                            
-        
-      // Click anywhere to close Modal
-        document.getElementById('myModal').addEventListener('click', () => { 
-          if(modal.style.display = 'block'){
-            modal.style.display = 'none';
-          }
-        });
-    
-    };
-  }
-  xhr.send();
-
-
